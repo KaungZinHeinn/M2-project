9bb65228481b2fb0b475d4fb41866b05c6f1859a
KaungZinHeinn <kaungzinhein245@gmail.com>
2024-10-06 10:25:08 +0630

    message

---
+++ ecom/store/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import *
+admin.site.register(Category)
+admin.site.register(Customers)
+admin.site.register(Order)
+admin.site.register(Product)
+
+# Register your models here.
